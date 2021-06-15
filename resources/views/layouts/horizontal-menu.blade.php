6b5da96995979d6ba639a444968b7174f134c236
MatheusFelipeMarinho <matheus_fellip@hotmail.com>
2021-06-15 14:45:40 -0300

    dev updates

--- resources/views/layouts/horizontal-menu.blade.php
+++ resources/views/layouts/horizontal-menu.blade.php
@@ -2,11 +2,18 @@
 
 <nav class="stroke mb-5">
     <ul>
-      <li><a href=""><i class="fas fa-tachometer-alt fa-lg mr-1 "></i></i>Dashboard</a></li>
+      <li><a href=""><img
+        class="img-fluid ml-4"
+        widht="30"
+        src="/images/money.png"
+        alt="The Logo"
+        style="opacity: 0.8"
+      />Dashboard</a></li>
+
       <li><a href="">Devedores</a></li>
       <li><a href="">Financeiro</a></li>
-      <li><a href="">More</a></li>
-      <li><a href="">Nice staff</a></li>
+      <li><a href="">Acordos</a></li>
+      <li><a href="">Configurações</a></li>
     </ul>
   </nav>
 </div>
